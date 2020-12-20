8859167d5bc408f75ca715d01e98a5778bb45727
Josh Kunz <josh@kunz.xyz>
2020-12-19 23:18:08 -0800

    Add 2020 Day 01 solutions

---
+++ 2020/01/p2.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs;
+use std::str::FromStr;
+use std::result::Result;
+
+fn solve(input: &Vec<i64>) -> Result<(i64, i64, i64), String> {
+    for i in input.into_iter() {
+        for j in input.into_iter() {
+            for x in input.into_iter() {
+                if i + j + x == 2020 {
+                    return Ok((*i, *j, *x));
+                }
+            }
+        }
+    }
+    return Err(String::from("No result found"));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input_raw = fs::read_to_string(&args[1]).expect("argument must be a valid string");
+
+    let mut input: Vec<i64> = Vec::new();
+    for elem in input_raw.split_whitespace() {
+        input.push(i64::from_str(elem).expect("invalid number"));
+    }
+
+    let nums = solve(&input).unwrap();
+
+    println!("Solution: {:?}", nums.0 * nums.1 * nums.2);
+}
